c2db29b003a37b924c0d5efcd6256f7281698fbd
Edward Ndiyo <52774709+Edwardndiyo@users.noreply.github.com>
2025-10-30 22:54:25 +0100

    fixing optional error

--- run.py
+++ run.py
@@ -1,15 +1,9 @@
-# from app import create_app
-
-# app = create_app()
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # ✅ must match nginx upstream
+    port = int(os.environ.get("PORT", 10000))  # ✅ must match nginx upstream
+    print(f"🚀 Starting Flask app on port {port}")  # Debug line
     app.run(host="0.0.0.0", port=port)
